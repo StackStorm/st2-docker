5bf7c7af08edeac850e21588fa8a89914712298d
Warren Van Winckel <warren.van.winckel@gmail.com>
2017-04-06 00:33:50 -0700

    Use env vars, not args to entrypoint.sh

--- st2-upstart/bin/entrypoint.sh
+++ st2-upstart/bin/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-ST2_USER=${1:-st2admin}
-ST2_PASSWORD=${2:-Ch@ngeMe}
+ST2_USER=${ST2_USER:-st2admin}
+ST2_PASSWORD=${ST2_PASSWORD:-Ch@ngeMe}
+RMQ_USER=${RABBITMQ_DEFAULT_USER:-admin}
+RMQ_PASS=${RABBITMQ_DEFAULT_PASS:-pwd123}
 
 # Create htpasswd file and login to st2 using specified username/password
 htpasswd -b /etc/st2/htpasswd ${ST2_USER} ${ST2_PASSWORD}
+
+crudini --set /etc/st2/st2.conf messaging url amqp://${RMQ_USER}:${RMQ_PASS}@rabbitmq:5672
 
 exec /sbin/init
