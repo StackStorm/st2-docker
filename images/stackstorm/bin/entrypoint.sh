3a9427a7d3dc8fc7390c372a0e0af4278e9e721f
Warren Van Winckel <warren.van.winckel@gmail.com>
2017-04-28 01:51:12 -0700

    Add transport_url and connection variables

--- images/stackstorm/bin/entrypoint.sh
+++ images/stackstorm/bin/entrypoint.sh
@@ -12,10 +12,14 @@ mkdir -p /root/.st2
 touch /root/.st2/config
 
 crudini --set /etc/st2/st2.conf messaging url amqp://${RMQ_USER}:${RMQ_PASS}@rabbitmq:5672
+
 crudini --set /root/.st2/config credentials username ${ST2_USER}
 crudini --set /root/.st2/config credentials password ${ST2_PASSWORD}
 
 crudini --set /etc/st2/st2.conf mistral api_url http://127.0.0.1:9101
 crudini --set /etc/st2/st2.conf mistral v2_base_url http://127.0.0.1:8989/v2
+
+crudini --set /etc/mistral/mistral.conf DEFAULT transport_url amqp://${RMQ_USER}:${RMQ_PASS}@rabbitmq:5672
+crudini --set /etc/mistral/mistral.conf database connection postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres/${POSTGRES_DB}
 
 exec /sbin/init
