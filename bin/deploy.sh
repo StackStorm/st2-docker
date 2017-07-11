d023f9a9538544f22f20409cf3eccd0762e10961
Warren Van Winckel <warren.van.winckel@gmail.com>
2017-07-10 19:07:13 -0700

    Ensure CircleCI can print env variables

---
+++ bin/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo DOCKER_USER=$DOCKER_USER
+echo DOCKER_PASSWORD=$DOCKER_PASSWORD
+
+exit 0
+
+docker login --username ${DOCKER_USER} --password ${DOCKER_PASSWORD} --email ${DOCKER_EMAIL}
+docker push stackstorm:stackstorm/${ST2_TAG}
+
+docker build --build-arg ST2_TAG=${ST2_TAG} -t stackstorm/stackstorm:${ST2_TAG} images/stackstorm
+
+if [ "$ST2_TAG" == "$LATEST" ]; then
+  # Also build the latest image
+  docker build --build-arg ST2_TAG=${ST2_TAG} -t stackstorm/stackstorm:latest images/stackstorm
+fi
