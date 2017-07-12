19cf68bc32d55a9af561a5d853b21886d0c0956f
Warren Van Winckel <warren.van.winckel@gmail.com>
2017-07-12 14:21:03 -0700

    Add comments + minor clean-up

--- bin/build-deploy.sh
+++ bin/build-deploy.sh
@@ -1,36 +1,79 @@
 #!/bin/bash
+#
+# This script runs within the CircleCI environment to build and deploy
+# st2-docker images to Docker Hub.
+#
+# Generally speaking, we only maintain the image containing the latest release
+# of StackStorm tagged in the st2-docker repo. To build an image with a
+# specific release of StackStorm, push an annotated tag of the format "vX.Y.Z",
+# where "v" is literal, and "X.Y.Z" is the release number. The tag will be made
+# available within CircleCI using the $CIRCLE_TAG environment variable.
+#
+# If $CIRCLE_TAG has zero length, then no tag was pushed. The following image
+# will be built:
+#
+#   stackstorm/stackstorm:X.Y.Z   (where X.Y.Z is the latest StackStorm release)
+#
+# If this image was already built, then it will be built again (using a
+# potentially different Dockerfile). The image is only guaranteed to contain
+# the specified StackStorm release.
+#
+# If X.Y.Z is the latest release, the following target image is set to refer
+# to the above image:
+#
+#   stackstorm/stackstorm:latest
 
-set -e
+set -euo pipefail
+IDS=$'\n\t'
 
-echo CIRCLE_TAG=${CIRCLE_TAG}
+if [ -z ${CIRCLE_SHA1:-} ]; then
+  echo "ERROR: CIRCLE_SHA1 is not defined."
+  echo "To resolve, run:"
+  echo "  $ export CIRCLE_SHA1=<commit_sha>"
+  echo "  $ $0"
+  exit 1
+fi
 
-LATEST=`git tag -l | sort -r | head -1`
-echo LATEST=${LATEST}
+echo CIRCLE_TAG=${CIRCLE_TAG:-}
+latest=`git tag -l | sort -r | head -1`
+echo latest=${latest}
+
+if [[ ${CIRCLE_TAG:-} =~ ^v(.+)$ ]]; then
+  # A tag was pushed, so we'll build an image using this specific release.
+  tag=${BASH_REMATCH[1]}
+else
+  # Build and tag an image using the latest StackStorm release
+  if [[ ${latest} =~ ^v(.+)$ ]]; then
+    tag=${BASH_REMATCH[1]}
+  else
+    echo "ERROR: Could not find a git tag in the st2-docker repo with format vX.Y.Z"
+    echo "To resolve, run:"
+    echo "  $ git co master"
+    echo "  $ git tag -a 'vX.Y.Z' -m 'Stamping X.Y.Z' HEAD"
+    echo "  $ git push --tags"
+    exit 1
+  fi
+fi
+
+echo tag=${tag}
+
+if [ -z ${DOCKER_USER:-} ] || [ -z ${DOCKER_PASSWORD:-} ] ; then
+  echo "ERROR: DOCKER_USER and/or DOCKER_PASSWORD are not defined."
+  echo "To resolve, run:"
+  echo "  $ export DOCKER_USER=<docker_login>"
+  echo "  $ export DOCKER_PASSWORD=<docker_password>"
+  echo "  $ $0"
+  exit 1
+fi
 
 docker login --username ${DOCKER_USER} --password ${DOCKER_PASSWORD}
 
-# If CIRCLE_TAG is not zero length and starts with "v", then build the specific tag
-# (this only happens after that StackStorm release)
-if [[ $CIRCLE_TAG =~ ^v(.+)$ ]]; then
-  ST2_TAG=${BASH_REMATCH[1]}
-  for name in stackstorm; do
-    docker build --build-arg ST2_TAG=${ST2_TAG} --build-arg ST2_DOCKER_SHA1=${CIRCLE_SHA1} \
-      -t stackstorm/${name}:${ST2_TAG} images/${name}
-    echo "docker push stackstorm/${name}:${ST2_TAG}"
-  done
-fi
-
-# If TAG is zero length, then only build the 'latest' image
-# (this usually happens when developing st2-docker)
-if [ -z $CIRCLE_TAG ]; then
-  # Build the latest image (using $LATEST version of ST2)
-  if [[ $LATEST =~ ^v(.+)$ ]]; then
-    ST2_TAG=${BASH_REMATCH[1]}
-  fi
-  echo ST2_TAG=${ST2_TAG}
-  for name in stackstorm stackstorm-all; do
-    docker build --build-arg ST2_TAG=${ST2_TAG} --build-arg ST2_DOCKER_SHA1=${CIRCLE_SHA1} \
-      -t stackstorm/${name}:latest images/${name}
+for name in stackstorm; do
+  docker build --build-arg ST2_TAG=${tag} --build-arg ST2_DOCKER_SHA1=${CIRCLE_SHA1} \
+    -t stackstorm/${name}:${tag} images/${name}
+  echo "docker push stackstorm/${name}:${tag}"
+  if [ "${tag}" == "${latest}" ]; then
+    docker tag stackstorm/${name}:${tag} stackstorm/${name}:latest
     echo "docker push stackstorm/${name}:latest"
-  done
-fi
+  fi
+done
