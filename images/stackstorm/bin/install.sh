575ea989aac6cb3a81b9af8a5e67657f12ccba98
Warren Van Winckel <warren.van.winckel@gmail.com>
2017-07-13 14:55:16 -0700

    Build and push dev image

--- images/stackstorm/bin/install.sh
+++ images/stackstorm/bin/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -euo pipefail
+IDS=$'\n\t'
+
 # Parse options
 while [[ "$#" > 1 ]]; do case $1 in
   --sha) ST2_DOCKER_SHA1="$2";;
@@ -11,14 +14,27 @@ while [[ "$#" > 1 ]]; do case $1 in
 esac; shift; shift;
 done
 
-if [ -z ${ST2_VERSION} ]; then
-  ST2_VERSION=`apt-cache madison st2 | cut -f 2 -d '|' | tr -d '[ \t]' | grep ${ST2_TAG} | head -1`
+# FIXME: De-duplicate following commands - initial effort to do so failed
+if [ -z ${ST2_VERSION:-} ]; then
+  if [[ -n ${ST2_TAG:-} ]]; then
+    ST2_VERSION="$(apt-cache madison st2 | cut -f 2 -d '|' | tr -d '[ \t]' | grep ${ST2_TAG} | head -1)"
+  else
+    ST2_VERSION="$(apt-cache madison st2 | cut -f 2 -d '|' | tr -d '[ \t]' | head -1)"
+  fi
 fi
-if [ -z ${ST2WEB_VERSION} ]; then
-  ST2WEB_VERSION=`apt-cache madison st2web | cut -f 2 -d '|' | tr -d '[ \t]' | grep ${ST2_TAG} | head -1`
+if [ -z ${ST2WEB_VERSION:-} ]; then
+  if [[ -n ${ST2_TAG:-} ]]; then
+    ST2WEB_VERSION="$(apt-cache madison st2web | cut -f 2 -d '|' | tr -d '[ \t]' | grep ${ST2_TAG} | head -1)"
+  else
+    ST2WEB_VERSION="$(apt-cache madison st2web | cut -f 2 -d '|' | tr -d '[ \t]' | head -1)"
+  fi
 fi
-if [ -z ${ST2MISTRAL_VERSION} ]; then
-  ST2MISTRAL_VERSION=`apt-cache madison st2mistral | cut -f 2 -d '|' | tr -d '[ \t]' | grep ${ST2_TAG} | head -1`
+if [ -z ${ST2MISTRAL_VERSION:-} ]; then
+  if [[ -n ${ST2_TAG:-} ]]; then
+    ST2MISTRAL_VERSION="$(apt-cache madison st2mistral | cut -f 2 -d '|' | tr -d '[ \t]' | grep ${ST2_TAG} | head -1)"
+  else
+    ST2MISTRAL_VERSION="$(apt-cache madison st2mistral | cut -f 2 -d '|' | tr -d '[ \t]' | head -1)"
+  fi
 fi
 
 MANIFEST="/st2-manifest.txt"
