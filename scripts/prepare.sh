17aaf5be355d8623d0e1de7ea891a3600b705297
Roman Valls Guimera <brainstorm@nopcode.org>
2017-11-28 11:57:41 +1100

    Docker daemon got 'masked' by systemcl after a redeployment, installing from upstream instead

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-apt-get install -y docker.io
-# Debian package heavily deprecated, as usual.
+# Debian package heavily deprecated, as usual. Install docker-ce from upstream instead
+#apt-get install -y docker.io
+curl -fsSL get.docker.com -o get-docker.sh
+sh get-docker.sh && rm get-docker.sh
+usermod -aG docker ubuntu
+
+# Install docker compose
 curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 gpasswd -a ubuntu docker
