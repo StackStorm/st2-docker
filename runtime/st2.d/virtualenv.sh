9c6252243824e91c98f39bb03a4adce760973b1c
Dmitri Zimine <dz@stackstorm.com>
2018-01-30 09:13:41 -0800

    Replace tab fro spaces.

--- runtime/st2.d/virtualenv.sh
+++ runtime/st2.d/virtualenv.sh
@@ -2,7 +2,7 @@
 EXAMPLES=/opt/stackstorm/packs/examples
 
 if [ ! -d "$EXAMPLES" ]; then
-	echo "Installing examples..."
+  echo "Installing examples..."
   cp -R /usr/share/doc/st2/examples /opt/stackstorm/packs
   chgrp -R st2packs /opt/stackstorm/packs/examples
   st2 run packs.setup_virtualenv packs=examples
