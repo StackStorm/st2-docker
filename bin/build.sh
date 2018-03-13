7c6476239771e948954eb3b8e714e003eee985a0
Warren Van Winckel <warren.van.winckel@gmail.com>
2018-03-13 16:41:58 -0700

    Reduce indentation

--- bin/build.sh
+++ bin/build.sh
@@ -8,44 +8,43 @@ IDS=$'\n\t'
 source bin/common.sh
 
 for name in stackstorm; do
-  if [ -z ${BUILD_DEV} ]; then
-    # This is not a dev build
-    st2_tag=${tag}
-
-    if [ -z ${CIRCLE_TAG} ]; then
-      # A tag was not pushed, so we only need to build 'latest' (not tagged)
-      tag='latest'
-    fi
-
-    name_tag="${name}:${tag}"
-
-    # Build the image, tag using CIRCLE_TAG
-    ${dry_run} docker build --build-arg ST2_TAG=${st2_tag} --build-arg CIRCLE_SHA1=${CIRCLE_SHA1} \
-      --build-arg CIRCLE_PROJECT_USERNAME=${CIRCLE_PROJECT_USERNAME:-} \
-      --build-arg CIRCLE_PROJECT_REPONAME=${CIRCLE_PROJECT_REPONAME:-} \
-      --build-arg CIRCLE_BUILD_URL=${CIRCLE_BUILD_URL:-} \
-      -t stackstorm/${name_tag} images/${name}
-
-    if [ "v${tag}" == "${latest}" ]; then
-      ${dry_run} docker tag stackstorm/${name_tag} stackstorm/${name}:${short_tag:-}
-    else
-      echo "INFO: Short tag is unchanged since this is not a tagged build."
-    fi
-
-    if [ "$tag" != 'latest' ]; then
-      ${dry_run} docker tag stackstorm/${name_tag} stackstorm/${name}:latest
-    fi
-  else
+  if [ ! -z ${BUILD_DEV} ]; then
     # Triggered to run nightly via ops-infra
     # Build unstable, and tag as "dev".
-
-    # TODO: Potentially useful to prepend "dev" with revision of latest unstable
-    #       release (e.g. "2.4dev")
 
     ${dry_run} docker build --build-arg ST2_REPO=unstable --build-arg CIRCLE_SHA1=${CIRCLE_SHA1} \
       --build-arg CIRCLE_PROJECT_USERNAME=${CIRCLE_PROJECT_USERNAME:-} \
       --build-arg CIRCLE_PROJECT_REPONAME=${CIRCLE_PROJECT_REPONAME:-} \
       --build-arg CIRCLE_BUILD_URL=${CIRCLE_BUILD_URL:-} \
       -t stackstorm/${name}:dev images/${name}
+
+    continue
+  fi
+
+  # This is not a dev build
+  st2_tag=${tag}
+
+  if [ -z ${CIRCLE_TAG} ]; then
+    # A tag was not pushed, so we only need to build 'latest' (not tagged)
+    tag='latest'
+  fi
+
+  name_tag="${name}:${tag}"
+
+  # Build the image, tag using CIRCLE_TAG
+  ${dry_run} docker build --build-arg ST2_TAG=${st2_tag} --build-arg CIRCLE_SHA1=${CIRCLE_SHA1} \
+    --build-arg CIRCLE_PROJECT_USERNAME=${CIRCLE_PROJECT_USERNAME:-} \
+    --build-arg CIRCLE_PROJECT_REPONAME=${CIRCLE_PROJECT_REPONAME:-} \
+    --build-arg CIRCLE_BUILD_URL=${CIRCLE_BUILD_URL:-} \
+    -t stackstorm/${name_tag} images/${name}
+
+  if [ "v${tag}" == "${latest}" ]; then
+    ${dry_run} docker tag stackstorm/${name_tag} stackstorm/${name}:${short_tag:-}
+  else
+    echo "INFO: Short tag is unchanged since this is not a tagged build."
+  fi
+
+  if [ "$tag" != 'latest' ]; then
+    ${dry_run} docker tag stackstorm/${name_tag} stackstorm/${name}:latest
   fi
 done
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,30 +9,33 @@ IDS=$'\n\t'
 source bin/common.sh
 
 for name in stackstorm; do
-  if [ -z ${BUILD_DEV} ]; then
-    # This is not a dev build!
-
-    # Push the tag to docker hub if and only if this is a tagged build.
-    # ASSUMPTION: Builds are never "re-tagged".
-    if [ ! -z ${CIRCLE_TAG} ]; then
-      if [ "${CIRCLE_TAG}" == "${latest}" ]; then
-        # Update latest if and only if the tag is the most recent tag.
-        # ASSUMPTION: Tags are applied in monotonically increasing order.
-        ${dry_run} docker push stackstorm/${name}:${tag}
-        if [ ! -z "${short_tag}" ]; then
-          ${dry_run} docker push stackstorm/${name}:${short_tag}
-        fi
-      else
-        echo "Not deploying image. ${CIRCLE_TAG} != ${latest}"
-      fi
-    else
-      ${dry_run} docker push stackstorm/${name}:latest
-    fi
-  else
+  if [ ! -z ${BUILD_DEV} ]; then
     # Build unstable, and tag as "dev".
 
     # TODO: Potentially useful to prepend "dev" with revision of latest unstable
     #       release (e.g. "2.4dev")
     ${dry_run} docker push stackstorm/${name}:dev
+    continue
+  fi
+
+  echo "not a dev build"
+
+  # This is not a dev build!
+
+  # Push the tag to docker hub if and only if this is a tagged build.
+  # ASSUMPTION: Builds are never "re-tagged".
+  if [ ! -z ${CIRCLE_TAG} ]; then
+    if [ "${CIRCLE_TAG}" == "${latest}" ]; then
+      # Update latest if and only if the tag is the most recent tag.
+      # ASSUMPTION: Tags are applied in monotonically increasing order.
+      ${dry_run} docker push stackstorm/${name}:${tag}
+      if [ ! -z "${short_tag}" ]; then
+        ${dry_run} docker push stackstorm/${name}:${short_tag}
+      fi
+    else
+      echo "Not deploying image. ${CIRCLE_TAG} != ${latest}"
+    fi
+  else
+    ${dry_run} docker push stackstorm/${name}:latest
   fi
 done
