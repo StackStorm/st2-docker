4642e138073110880676c4ef7561501b5bd99034
armab <dev@arma.biz>
2020-06-23 19:08:15 +0100

    Workaround Docker services startup wait before tests

--- st2client-startup.sh
+++ st2client-startup.sh
@@ -3,12 +3,10 @@
 # st2client startup and registration
 while true; do
   ACTIONS=$(st2 action list)
-  if [ "$?" -ne 0 ]
-  then
+  if [ "$?" -ne 0 ]; then
     echo "unable to reach downstream, will try again"
     sleep 5
-  elif [ "$ACTIONS" == "No matching items found" ]
-  then
+  elif [ "$ACTIONS" == "No matching items found" ]; then
     echo "No packs registered, will register"
     st2 pack register
   else
